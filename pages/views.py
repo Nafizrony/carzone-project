a0144682ee293e072b5f4474a23ea378ecdbb461
Nafiz <nafizfuadeng58@gmail.com>
2023-05-01 12:48:54 +0600

    All the pages dynamically linked

--- pages/views.py
+++ pages/views.py
@@ -5,3 +5,19 @@ from django.shortcuts import render
 def home(request):
     context = {}
     return render(request,'pages/home.html',context)
+
+def cars(request):
+    context = {}
+    return render(request,'pages/cars.html',context)
+
+def about(request):
+    context = {}
+    return render(request,'pages/about.html',context)
+
+def services(request):
+    context = {}
+    return render(request,'pages/services.html',context)
+
+def contact(request):
+    context = {}
+    return render(request,'pages/contact.html',context)
